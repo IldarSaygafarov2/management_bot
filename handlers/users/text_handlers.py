60c7d0b9ef2e0a8d1274359833795121ff1ca393
SkriLL <timurkazah1998@gmail.com>
2023-04-26 16:49:41 +0500

    added functions to recieve teacher name

--- handlers/users/text_handlers.py
+++ handlers/users/text_handlers.py
@@ -22,18 +22,30 @@ def get_name(message: types.Message):
 def get_lastname(message: types.Message, first_name: str):
     chat_id = message.from_user.id
     last_name = message.text.title()
-    bot.send_message(chat_id, text=messages.USER_COURSE_MESSAGE, reply_markup=kb.show_courses_menu())
+    bot.send_message(chat_id, text=messages.USER_COURSE_MESSAGE,
+                     reply_markup=kb.show_courses_menu())
     bot.register_next_step_handler(message, get_course, first_name, last_name)
 
 
 def get_course(message: types.Message, first_name: str, last_name: str):
     chat_id = message.from_user.id
     course = message.text
-    bot.send_message(chat_id, text=messages.USERS_TEACHER_MESSAGE, reply_markup=kb.show_teachers_menu())
-    bot.register_next_step_handler(message, get_teacher, first_name, last_name, course)
+    bot.send_message(chat_id, text=messages.USERS_TEACHER_MESSAGE,
+                     reply_markup=kb.show_teachers_menu())
+    bot.register_next_step_handler(
+        message, get_teacher, first_name, last_name, course)
 
 
 def get_teacher(message: types.Message, first_name: str, last_name: str, course: str):
     chat_id = message.from_user.id
-    print(first_name, last_name, course, message.text)
+    teacher = message.text
+    bot.send_message(chat_id,
+                     text=messages.LESSON_DAYS_MESSAGE,
+                     reply_markup=types.ReplyKeyboardRemove())
+    bot.register_next_step_handler(
+        message, get_lesson_days, first_name, last_name, course, teacher)
 
+
+def get_lesson_days(message: types.Message, first_name: str, last_name: str, course: str, teacher: str):
+    chat_id = message.from_user.id
+    
